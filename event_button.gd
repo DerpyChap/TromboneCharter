186588f758b168d8bebeafbb5474b84416f9e8f6
DerpyChap <holla@derpychap.co.uk>
2024-07-25 18:48:09 +0100

    minor tweaks to event previewer

--- event_button.gd
+++ event_button.gd
@@ -5,7 +5,9 @@ extends Button
 @export var event_id := 0
 
 func _ready():
-	pressed.connect(on_pressed)
+    pressed.connect(on_pressed)
+    tooltip_text = str(event_id)
+    text += " (" + str(event_id) + ")"
 
 func on_pressed():
-	%EventsAPI.send_event(event_id)
+    %EventsAPI.send_event(event_id)
